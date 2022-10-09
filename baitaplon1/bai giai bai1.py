458ced2f5270a914fa93f78449b1fd2916d3a6e7
Hiep <tranhoanghiep1102@gmail.com>
2022-10-09 20:58:13 +0900

    bai giai bai tap lon 1

---
+++ baitaplon1/bai giai bai1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 20:52:46 2022
+Bai giai bai tap lon 1
+@author: Hiep Tran
+"""
+import numpy
+x = int(input("Enter number x : "))
+y = int(input("Enter number y : "))
+print(x**y)
+numpy.log2(8)
